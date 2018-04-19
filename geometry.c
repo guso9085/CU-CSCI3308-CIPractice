b3c973003908bc237ccf26a1d4065d6a63a8c512
Gustav Solis <guso9085@colorado.edu>
2018-04-19 16:46:36 -0600

    ohboy

--- geometry.c
+++ geometry.c
@@ -20,7 +20,8 @@
 #define DEBUG(file, line, func, msg) fprintf(stderr, "DEBUG - %s_%d_%s: %s", file, line, func, msg);
 
 double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
-    return fabs((a->x * (b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y)) / 2);
+    //length of sides divided by 2
+    return fabs( (a->x * (b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y) ) / 2);
 }
 
 
